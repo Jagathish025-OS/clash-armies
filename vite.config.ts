740e5f88f782ff9fc9de4a9fd12fcd30cf4336a5
Jagathish025-OS <jaga31699@gmail.com>
2026-09-21 11:19:57 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -19,11 +19,6 @@ export default defineConfig((env) => {
 	const plugins: PluginOption[] = [
 		sveltekit({
 			preprocess: vitePreprocess(),
-
-			paths: {
-				base: '/COC',
-			},
-
 			adapter: adapter({
 				// Where the final node build that uses the vite build goes.
 				// This is the code that gets deployed to prod.
@@ -63,13 +58,7 @@ export default defineConfig((env) => {
 		test: {
 			include: ['tests/app/**/*.ts'],
 			coverage: {
-				exclude: [
-					'build/**',
-					'scripts/**',
-					'discord/**',
-					'svelte.config.js',
-					...coverageConfigDefaults.exclude,
-				],
+				exclude: ['build/**', 'scripts/**', 'discord/**', 'svelte.config.js', ...coverageConfigDefaults.exclude],
 			},
 			fileParallelism: false,
 		},
